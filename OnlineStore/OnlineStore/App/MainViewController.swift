d903419d1b6d383cecfec7cf0fbec23904ae0f97
Administration <administration@Administrations-MacBook-Air.local>
2025-09-29 13:14:11 +0500

    Add initial project structure

--- OnlineStore/OnlineStore/App/MainViewController.swift
+++ OnlineStore/OnlineStore/App/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
