2deaed0720c463a4943ca2e29d38779dfb1bde25
Administration <administration@Administrations-MacBook-Air.local>
2025-10-19 21:46:14 +0500

    feat: implement full Manager screen with Products and Categories sections

---
+++ OnlineStore/OnlineStore/Screens/TabBar/Manager/AddNewProductViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewProductViewController.swift
+//  OnlineStore
+//
+//  Created by Administration  on 19/10/25.
+//
+
+
+import UIKit
+
+class AddNewProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        self.title = "Add Product"
+    }
+}
---
+++ OnlineStore/OnlineStore/Screens/TabBar/Manager/UpdateProductViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateProductViewController.swift
+//  OnlineStore
+//
+//  Created by Administration  on 19/10/25.
+//
+
+
+import UIKit
+
+class UpdateProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+        self.title = "Update Product"
+    }
+}
